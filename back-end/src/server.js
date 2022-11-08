559e84437dbbde33488732cbf0f9167df521c702
Tales Rodrigues <tales00001@gmail.com>
2022-11-08 07:23:36 -0300

    Deploying Back-End

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,4 +2,4 @@ require('dotenv');
 const app = require('./app');
 const port = process.env.PORT;
 
-app.listen(port, () => console.log(`Runing at localhost:${port}`));
+app.listen(port, () => console.log(`Running at localhost:${port}`));
